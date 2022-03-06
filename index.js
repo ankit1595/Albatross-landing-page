3f32ac07cb7c1279aa152b452047d1ea716626a8
Ankit <ankit.95sharma@gmail.com>
2022-03-06 20:38:06 +0530

    added clickevent and quote section

---
+++ index.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+var nonFiction=document.getElementById("non-fic")
+var ficNonSection = document.getElementById("non-fiction");
+var ficSection = document.getElementById("fiction");
+nonFiction.addEventListener('click',()=>{
+    ficNonSection.style.display="block";
+    ficSection.style.display="none";
+})
+
+var fiction=document.getElementById("fic");
+console.log(fiction);
+fiction.addEventListener('click',()=>{
+    ficSection.style.display="block";
+    ficNonSection.style.display="none";
+})
+
+// var body1=document.getElementsByTagName('section')
+// body1.addEventListener('click',()=>{
+//     e.preventDefault();
+//     ficNonSection.style.display="none";
+//     ficSection.style.display="none";
+// })
